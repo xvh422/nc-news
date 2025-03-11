b36d6935451e537feb8cae112224f018232c5075
xvh422 <joey.carter555@gmail.com>
2025-03-11 11:42:33 +0000

    Ticket 8 Complete

--- models/comments.models.js
+++ models/comments.models.js
@@ -43,10 +43,12 @@ exports.createNewComment = (username, body, article_id) => {
 
 exports.removeComment = (comment_id) => {
   return db
-    .query("DELETE FROM comments WHERE comment_id = $1 RETURNING *", [comment_id])
+    .query("DELETE FROM comments WHERE comment_id = $1 RETURNING *", [
+      comment_id,
+    ])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Comment not found" });
       }
-    })
+    });
 };
