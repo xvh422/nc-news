c8939f0622274d6734a669e7e3ef1f67ddddc5b5
xvh422 <joey.carter555@gmail.com>
2025-03-12 14:27:22 +0000

    Updated Connection File

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,10 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
-} else {
+} else if (process.env.PGDATABASE) {
   console.log(`Connected to ${process.env.PGDATABASE}`);
+} else {
+  console.log(`Connected to ${process.env.DATABASE_URL}`);
 }
 
 const config = {};
