6fab0ad64cdb89e918c5318f9211aa6a5a3d617e
xvh422 <joey.carter555@gmail.com>
2025-03-13 10:43:30 +0000

    Ticket 17 Complete

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,6 +2,7 @@ const {
   fetchCommentsByArticleId,
   createNewComment,
   removeComment,
+  updateComment,
 } = require("../models/comments.models.js");
 
 exports.getCommentsByArticleId = (req, res, next) => {
@@ -28,6 +29,16 @@ exports.deleteComment = (req, res, next) => {
   removeComment(comment_id)
     .then(() => {
       res.status(204).send();
+    })
+    .catch(next);
+};
+
+exports.patchComment = (req, res, next) => {
+  const { comment_id } = req.params;
+  const { inc_votes } = req.body;
+  updateComment(inc_votes, comment_id)
+    .then((comment) => {
+      res.status(200).send({ comment });
     })
     .catch(next);
 };
--- routers/comments.router.js
+++ routers/comments.router.js
@@ -1,6 +1,9 @@
 const commentsRouter = require("express").Router();
-const { deleteComment } = require("../controllers/comments.controllers.js");
+const {
+  deleteComment,
+  patchComment,
+} = require("../controllers/comments.controllers.js");
 
-commentsRouter.route("/:comment_id").delete(deleteComment);
+commentsRouter.route("/:comment_id").patch(patchComment).delete(deleteComment);
 
 module.exports = commentsRouter;
