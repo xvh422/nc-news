3c2d335d2aa6f31c2be5506f178564e5863cb68a
xvh422 <joey.carter555@gmail.com>
2025-03-14 11:38:40 +0000

    Ticket 21 Complete

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,21 @@
-const { fetchAllTopics } = require("../models/topics.models.js");
+const {
+  fetchAllTopics,
+  createNewTopic,
+} = require("../models/topics.models.js");
 
 exports.getAllTopics = (req, res, next) => {
   fetchAllTopics()
     .then((topics) => {
       res.status(200).send({ topics });
+    })
+    .catch(next);
+};
+
+exports.postNewTopic = (req, res, next) => {
+  const { slug, description } = req.body;
+  createNewTopic(slug, description)
+    .then((topic) => {
+      res.status(201).send({ topic });
     })
     .catch(next);
 };
--- models/topics.models.js
+++ models/topics.models.js
@@ -5,3 +5,14 @@ exports.fetchAllTopics = () => {
     return rows;
   });
 };
+
+exports.createNewTopic = (slug, description) => {
+  return db
+    .query("INSERT INTO topics (slug, description) VALUES ($1, $2) RETURNING *", [
+      slug,
+      description,
+    ])
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
